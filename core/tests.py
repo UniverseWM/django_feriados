0f2ed07da459bc9777173679835affe95551c9a4
Wilkne Maia <72803395+wilkneMaia@users.noreply.github.com>
2022-10-16 18:45:39 -0300

    feriado v.1

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class NatalTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/')
+
+    def test_200_esponse(self):
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_exto(self):
+        self.assertContains(self.resp, 'Natal')
--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def natal(request):
+    return HttpResponse("<h1><center>Não é Natal!</center></h1>")
